a77448714067cbe77204153f5e3c565c0407416c
taisan11 <131413638+taisan11@users.noreply.github.com>
2024-08-31 08:57:39 +0900

    away

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import {lineNotify} from "./linenotify.ts"
 
 const bot = createBot({
   token: Deno.env.get("DISCOED_TOKEN")!,
-  intents: Intents.Guilds | Intents.GuildMessages, // Or other intents that you might needs.
+  intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent, // Or other intents that you might needs.
   events: {
     ready: data => {
       console.log(`The shard ${data.shardId} is ready!`)
@@ -13,15 +13,21 @@ const bot = createBot({
 })
 
 // You can add events after the createBot call if you prefer
+bot.transformers.desiredProperties.message.guildId = true
+bot.transformers.desiredProperties.message.channelId = true
+bot.transformers.desiredProperties.message.content = true
 
 bot.events.messageCreate = message => {
+  console.log("messageCreate")
   if (!(message.guildId == BigInt(Deno.env.get("DISCORD_GUILD")!))) {
     return
   }
   if (!(message.channelId === BigInt(Deno.env.get("DISCORD_CHANNEL")!))) {
     return
   }
-  lineNotify({message: message.content, token: Deno.env.get("LINE_NOTIFY")!})
+  console.log("送信!!")
+  console.log(message.content)
+  lineNotify({message: message.content, token: Deno.env.get("LINE_Notification_Token")!})
 }
 
 await bot.start()
