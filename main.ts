ef3d19abed958d6c07692f86e40aa1cf22b3abe8
taisan11 <131413638+taisan11@users.noreply.github.com>
2024-09-01 08:05:03 +0900

    これが正しいはず

--- main.ts
+++ main.ts
@@ -68,7 +68,8 @@ const textEventHandler = async (
   event: WebhookEvent,
 ): Promise<MessageAPIResponseBase | undefined> => {
   console.log(event.source.userId)
-  if (event.type !== "message" || event.message.type !== "text" || event.source.userId === "false") {
+  //@ts-ignore: 2339??
+  if (event.type !== "message" || event.message.type !== "text" || event.source.userId === false) {
     return;
   }
   await bot.helpers.sendMessage(BigInt(Deno.env.get("DISCORD_CHANNEL")!), { content: event.message.text })
